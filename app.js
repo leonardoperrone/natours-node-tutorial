e8ee855359da455161a32befa320af79013d89f3
Leonardo Perrone <liox2012@gmail.com>
2019-10-06 15:17:54 +0200

    Implemented CORS

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -30,6 +31,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // GLOBAL MIDDLEWARE
+// Implemente cors, Access-Control-Allow-Origin *
+app.use(cors());
+
+// NOTE: with this below we would allow our frontend to access our api on the server
+// app.use(cors({
+//   origin: 'frontend domain'
+// }))
+
+app.options('*', cors());
+// To allow cors on a specific path do the following
+// app.options('/api/v1/tours/:id', cors());
+
 // Set security HTTP headers
 app.use(helmet());
 
