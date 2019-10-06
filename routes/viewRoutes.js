92bf2fdb16734031ade8e7aa768e39aec5380c21
Leonardo Perrone <liox2012@gmail.com>
2019-10-06 17:20:11 +0200

    Removed line from view routes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,8 +4,6 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.use(viewsController.alerts);
-
 router.get('/', authController.isLoggedIn, viewsController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
